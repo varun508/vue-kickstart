70d4a0913b63b08a5b990a973590056fb4f77718
Varun <imva508@gmail.com>
2019-03-26 13:53:26 +0530

    added some style to the cards

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" style="background-color: #eeeeee">
     <Todos v-bind:todos="todos"></Todos>
   </div>
 </template>
--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,12 +1,9 @@
 <template>
   <div>
-    <div>
-      <br>
+    <div class="card">
       <h3>{{ todo.task }}</h3>
       <p v-if="todo.completed">This is completed</p>
       <p v-if="!todo.completed">This is not completed</p>
-      <br>
-      <hr>
     </div>
   </div>
 </template>
@@ -19,5 +16,22 @@ export default {
 </script>
 
 <style>
+.card {
+  padding: 50px;
+  border-radius: 12px;
+  display: inline-block;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.15);
+  margin: 20px;
+  position: relative;
+  background-color: white;
+  margin-bottom: 50px;
+  transition: all 0.2s ease-in-out;
+}
+
+.card:hover {
+  /*box-shadow: 0 5px 22px 0 rgba(0,0,0,.25);*/
+  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
+  margin-bottom: 54px;
+}
 </style>
 
