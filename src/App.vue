259819cbf6e65090cfb2a37f3d27690216cd9621
Varun <imva508@gmail.com>
2019-03-26 12:58:28 +0530

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,49 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Todos></Todos>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Todos from "./components/Todos";
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Todos
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          todo: "Task number 1",
+          completed: false
+        },
+        {
+          id: 2,
+          todo: "Task number 2",
+          completed: true
+        },
+        {
+          id: 3,
+          todo: "Task number 3",
+          completed: false
+        },
+      ]
+    };
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: Roboto;
+  line-height: 1.4;
 }
 </style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Todos</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"Todo"
+};
+</script>
+
+<style scoped>
+</style>
